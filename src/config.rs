10e604268763f353241d2d7f14c8eef724415aad
ONER-KY <161009863+ONER-KY@users.noreply.github.com>
2025-08-06 12:52:28 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["182.92.67.163"];
+pub const RS_PUB_KEY: &str = "QMKRTtWrBl5raAI5p6vDBOiuzUJKQNEi3r7KYbPkryU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
